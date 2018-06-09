3c4b2afc96bb709070ade4f9090e7d12bfd453b0
nileshlg2003 <nileshlg2003@gmail.com>
2018-06-10 01:34:33 +0530

    Setup asic Express, Added Jade templates for About

--- app.js
+++ app.js
@@ -5,8 +5,32 @@ var nodeMailer = require("nodemailer");
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res){
+
+    //console.log("Hello world");
+    res.render('index', {title:"Welcome"});
+
+});
+app.get('/about', function(req, res){
+
+    //console.log("Hello world");
+    res.render('about');
+
+});
+app.get('/contact', function(req, res){
+
+    //console.log("Hello world");
+    res.render('contact');
+
+});
 
 app.listen(3000);
 console.log("Server is running on port 3000 .....");
